00cd5c9d4ac17765ad7f3f9e79e03aa1658292a4
jdl <1265105759@qq.com>
2022-08-29 18:31:13 +0800

    修改

---
+++ cloudalibaba-consumer-nacos-order84/src/test/java/com/example/cloudalibabaconsumernacosorder84/CloudalibabaConsumerNacosOrder84ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudalibabaconsumernacosorder84;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudalibabaConsumerNacosOrder84ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
